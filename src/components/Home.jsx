ac31e2704bc37b5bfab22f94b0b6747b247f3616
Faccin27 <gfaccin27@gmail.com>
2024-09-03 17:14:57 -0300

    feat: create landing page for first user impression

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="bg-black">
+    <div className="bg-white">
       {/* Header */}
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
@@ -114,6 +114,68 @@ export default function Example() {
           </DialogPanel>
         </Dialog>
       </header>
+      <main>
+        {/* Hero section */}
+        <div className="relative isolate overflow-hidden bg-gray-900 pb-16 pt-14 sm:pb-20">
+          <img
+            alt=""
+            src={bk}
+            className="absolute inset-0 -z-10 h-full w-full object-cover opacity-10"
+          />
+          <div
+            aria-hidden="true"
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
+                  Trial test for free.{" "}
+                  <a href="#" className="font-semibold text-white">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Read more <span aria-hidden="true">&rarr;</span>
+                  </a>
+                </div>
+              </div>
+              <div className="text-center">
+                <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                  Supremacy Cheats <br />
+                  <span className="text-3xl"> The best way to high level </span>
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-300">
+                  Tired of losing? Frustrated with always finishing last in your
+                  matches? Try out the new game mode called Supremacy. Being
+                  Immortal is the least you can expect.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <a
+                    href="#"
+                    className="rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                    style={{ borderRadius: 8 }}
+                  >
+                    Get started
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm font-semibold leading-6 text-white hover:bg-zinc-300 hover:bg-opacity-35 px-3.5 py-2.5"
+                    style={{ borderRadius: 8 }}
+                  >
+                    Live demo <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
