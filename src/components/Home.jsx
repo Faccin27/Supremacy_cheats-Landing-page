23eec8cf4741ae861d5c672dd182edebe9a4e8bf
Faccin27 <gfaccin27@gmail.com>
2024-09-05 20:40:14 -0300

    feat: add feature items below menu photo to detail product basics

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -287,6 +287,22 @@ export default function Example() {
               </div>
             </div>
           </div>
+          <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+            <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+              {features.map((feature) => (
+                <div key={feature.name} className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <feature.icon
+                      aria-hidden="true"
+                      className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                    />
+                    {feature.name}
+                  </dt>{" "}
+                  <dd className="inline">{feature.description}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
         </div>
       </main>
     </div>
